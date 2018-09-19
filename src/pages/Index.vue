f86433fc8fbc194a5a6b3c2dd75069de6b41f640
Meena.Hoda <meena.hoda@wmfs.net>
2018-09-19 14:31:25 +0100

    use virtual keyboard rather than text input (styling needs improving)

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,115 +1,105 @@
 <template>
   <q-page padding>
-    <div class="q-mx-xl">
-      <div class="q-display-3 q-mb-xl text-white row">Hangman!</div>
-      <div class="row q-mb-xl">
-        <div class="col-sm-12 col-md-6">
-          <div class="q-display-1 text-white">
-            <span v-for="(letter, idx) in revealed" :key="idx" class="q-mr-sm">
-              <span v-if="letter === null">_</span>
-              <span v-else :class="missingIdxs.includes(idx) ? 'text-red' : success ? 'text-green' : 'text-white'">{{letter}}</span>
-            </span>
-          </div>
-        </div>
-        <div class="col-sm-12 col-md-6">
-          <div class="q-display-1 text-white">{{maxAttempts - attempts}} attempts left.</div>
-          <div v-if="graveyard.length > 0" class="text-white">
-            Used letters: <span v-for="letter in graveyard" :key="letter">{{letter}} </span>
-          </div>
-        </div>
-      </div>
-    
-      <q-input
-        dark
-        color="info"
-        float-label="Type a letter"
-        v-model="attempt"
-        @keyup.enter="tryLetter"
-        :maxlength="1"
-        :disable="disable"
-      />
 
-      <q-btn color="info" label="Go" @click="tryLetter" class="q-mt-lg q-mr-sm"/>
-      <q-btn color="info" outline label="Reset" @click="reset" class="q-mt-lg" />
+    <div class="q-display-1 text-white text-center">
+      <span v-for="(letter, idx) in revealed" :key="idx" class="q-mr-sm">
+        <span v-if="letter === null">_</span>
+        <span v-else :class="missingIdxs.includes(idx) ? 'text-red' : success ? 'text-green' : 'text-white'">{{letter}}</span>
+      </span>
+    </div>
+
+    <div class="q-subheading q-mt-xl text-white text-center">{{maxAttempts - attempts}} attempts left</div>
+
+    <div class="row justify-center q-mt-xl" id="keyboard">
+      <div
+        v-for="(char, id) in alphabet"
+        :key="`char-${id}`"
+        class="col-sm-1"
+      >
+        <q-btn :color="graveyard.includes(char) ? '' : 'secondary'" class="q-mt-sm q-mr-sm" :label="char" @click="tryLetter(char)" :disable="graveyard.includes(char) || disable || succcess" />
+      </div>
+    </div>
+
+    <div class="text-center q-mt-xl">
+      <q-btn color="negative" outline label="Reset" @click="reset" />
     </div>
   </q-page>
 </template>
 
+<style>
+@media only screen and (min-width: 992px) {
+  #keyboard {
+    width: 60%;
+    margin: 48px auto 0 auto;
+  }
+}
+
+@media only screen and (min-width: 1200px) {
+  #keyboard {
+    width: 40%;
+    margin: 48px auto 0 auto;
+  }
+}
+</style>
+
 <script>
-// TODO: html maxlength does not work on mobile keyboard
 import words from 'assets/words.json'
 export default {
   name: 'PageIndex',
   data () {
     return {
+      alphabet: 'abcdefghijklmnopqrstuvwxyz',
       maxAttempts: 8,
       chosenWord: '',
       revealed: [],
       graveyard: [],
       attempts: 0,
-      attempt: '',
       disable: false,
       success: false,
       missingIdxs: []
     }
   },
   methods: {
-    tryLetter () {
-      if (this.attempt.trim().length === 1 && this.attempt.trim().match(/[a-z]/i)) {
-        this.attempt = this.attempt.toLowerCase()
-        if (this.graveyard.includes(this.attempt)) {
+    tryLetter (char) {
+      this.graveyard.push(char)
+
+      if (this.chosenWord.includes(char)) {
+        this.chosenWord
+          .split('')
+          .forEach((letter, idx) => {
+            if (letter === char) this.revealed[idx] = char
+          })
+        
+        let success = true
+        this.revealed.forEach(letter => {
+          if (letter === null) success = false
+        })
+
+        if (success) {
           this.$q.notify({
-            type: 'negative',
-            message: `You have already tried the letter '${this.attempt}'.`,
+            type: 'positive',
+            message: 'Success!',
             position: 'top'
           })
-        } else {
-          this.graveyard.push(this.attempt)
-          this.graveyard.sort()
-
-          if (this.chosenWord.includes(this.attempt)) {
-            this.chosenWord.split('').forEach((letter, idx) => {
-              if (letter === this.attempt) this.revealed[idx] = this.attempt
-            })
-            
-            let success = true
-            this.revealed.forEach(letter => {
-              if (letter === null) success = false
-            })
-
-            if (success) {
-              this.$q.notify({
-                type: 'positive',
-                message: 'Success!',
-                position: 'top'
-              })
-              this.disable = true
-              this.success = true
-            }
-          } else {
-            this.attempts++
-            if (this.attempts === this.maxAttempts) {
-              this.$q.notify({
-                type: 'negative',
-                message: 'Game over!',
-                position: 'top'
-              })
-              this.disable = true
-              this.revealed.forEach((letter, idx) => {
-                if (letter === null) this.missingIdxs.push(idx)
-              })
-              this.revealed = this.chosenWord
-            } else {
-              // Draw hangman
-            }
-          }
+          this.disable = true
+          this.success = true
         }
       } else {
-        this.$q.notify({
-          type: 'negative',
-          message: 'Only letters allowed.',
-          position: 'top'
-        })
+        this.attempts++
+        if (this.attempts === this.maxAttempts) {
+          this.$q.notify({
+            type: 'negative',
+            message: 'Game over!',
+            position: 'top'
+          })
+          this.disable = true
+          this.revealed.forEach((letter, idx) => {
+            if (letter === null) this.missingIdxs.push(idx)
+          })
+          this.revealed = this.chosenWord
+        } else {
+          // Draw hangman
+        }
       }
     },
     reset () {
@@ -117,7 +107,6 @@ export default {
       this.revealed = new Array(this.chosenWord.length).fill(null)
       this.graveyard = []
       this.attempts = 0
-      this.attempt = ''
       this.disable = false
       this.missingIdxs = []
       this.success = false
